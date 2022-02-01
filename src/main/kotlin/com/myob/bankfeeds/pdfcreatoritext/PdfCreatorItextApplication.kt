d7a408f5939408c00ffb43c4e6aa47a4d77b31c9
Joyce.Yeung <joyce.yeung@myob.com>
2022-02-01 11:17:10 +1100

    setup project

---
+++ src/main/kotlin/com/myob/bankfeeds/pdfcreatoritext/PdfCreatorItextApplication.kt
@@ -0,0 +1,11 @@
+package com.myob.bankfeeds.pdfcreatoritext
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PdfCreatorItextApplication
+
+fun main(args: Array<String>) {
+    runApplication<PdfCreatorItextApplication>(*args)
+}
---
+++ src/test/kotlin/com/myob/bankfeeds/pdfcreatoritext/PdfCreatorItextApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.myob.bankfeeds.pdfcreatoritext
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PdfCreatorItextApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
